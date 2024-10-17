bfb82f4391b45f7062a7bc690b095f4d3efa4088
Takigawa16 <regil99ariola@gmail.com>
2024-10-17 08:53:50 +0800

    Task_4.1.py

--- Task_4.1.py
+++ Task_4.1.py
@@ -11,6 +11,8 @@ def multiplication(num1, num2):
     return num1 * num2
 
 def division(num1, num2):
+    if num2 == 0:
+        return " Invalid Input Cannot Divide "
     return num1 / num2
 
 #User input
